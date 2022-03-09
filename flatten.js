5a0f35cefe65f4a973699e874578f706fbdfde93
MattSeligman <matt@modelitup.com>
2022-03-09 00:22:17 -0800

    This function flatten which will take in an array containing elements including nested arrays of elements, and return a flattened version of the array.

---
+++ flatten.js
@@ -0,0 +1,67 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  
+    const arrayOneLength = arrayOne.length;
+    const arrayTwoLength = arrayTwo.length;
+  
+    let result = false;
+          
+    // Check the Length of Arrays
+    if (arrayOneLength === arrayTwoLength) {
+      
+      for (let i = 0; i < arrayOneLength; i++) {
+      
+        if (arrayOne[i] === arrayTwo[i]) {
+                      
+          result = true;
+        } else {
+          result = false;
+        }
+      }
+      
+    } else {
+      result = false;
+    }
+    
+    return result;
+      
+  };
+      
+  const assertArraysEqual = function(result) {
+    
+    const errorMsg = `🛑 The arrays are not equal 🛑`;
+    const successMsg = `✅ The arrays are Equal ✅`;
+                
+    if (result) {
+      console.log(successMsg);
+    } else {
+      console.log(errorMsg);
+    }
+  };
+  
+  const flatten = function(array) {
+
+    let flattenedArray = [];
+
+    for(let value in array) {
+
+        if( !Array.isArray(array[value] ) ) {
+
+            flattenedArray.push(array[value]);
+
+        } else if( Array.isArray(array[value]) ) {
+
+            for(let subValue in array[value] ){
+
+                flattenedArray.push(array[value][subValue]);
+            }
+            
+        } 
+        
+    }
+
+    return flattenedArray;
+  }
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual( eqArrays( flatten([1, 2, [3, 4], 5, [6]]) , [ 1, 2, 3, 4, 5, 6 ] ) ) //Test to Pass

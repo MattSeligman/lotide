987cf276ee2f8480bad8c6e5dcd5db94e1361c15
MattSeligman <matt@modelitup.com>
2022-03-10 20:04:57 -0800

    This function findKey takes in an object and a callback. It scans the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.

--- findKey.js
+++ findKey.js
@@ -25,6 +25,27 @@ const findKey = function(object, callback) {
 };
 
 findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(
+  findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    }, x => x.stars === 2),
+    "noma"
+  );
+
+  findKey({
     "Blue Hill": { stars: 1 },
     "Akaleri":   { stars: 3 },
     "noma":      { stars: 2 },
@@ -37,10 +58,10 @@ findKey({
     findKey({
         "Blue Hill": { stars: 1 },
         "Akaleri":   { stars: 3 },
-        "noma":      { stars: 2 },
+        "noma":      { stars: 1 },
         "elBulli":   { stars: 3 },
-        "Ora":       { stars: 2 },
+        "Ora":       { stars: 1 },
         "Akelarre":  { stars: 3 }
       }, x => x.stars === 2),
-      "noma"
+      undefined
     );

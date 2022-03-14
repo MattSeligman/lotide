7b5ec7abaf38e158e9b99de8e83979d1a26ad83d
MattSeligman <matt@modelitup.com>
2022-03-14 10:09:37 -0700

    Removed Tests, and added module.exports on line 5

--- head.js
+++ head.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-
-  const errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  const successMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-      
-  if (actual === expected) {
-    console.log(successMsg);
-  } else {
-    console.log(errorMsg);
-  }
-    
-};
-
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;

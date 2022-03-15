00ee31b17510ab3e1bab5f43708ec6db85de922d
MattSeligman <matt@modelitup.com>
2022-03-14 17:11:55 -0700

    Refactored and added a assertArraysEqual Test

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
 
   const errorMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   const successMsg = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-    
+  
   if (actual === expected) {
     console.log(successMsg);
   } else {
